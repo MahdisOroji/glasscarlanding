f6e51e402cf8a18ae2ba6a07829cb58aba5176f7
Mahdis Oroji <mahdis.oroji.1813@gmail.com>
2025-07-02 12:14:17 +0330

    items of header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Styled from "../Header/Header.module.css"
+
+function Header() {
+    return (
+
+        <div className={Styled.headerWrapper}>
+            <div className="container">
+                <ul className={Styled.header}>
+                    <li> <a href="#" className={Styled.headerLink}> تماس </a> </li>
+                    <li> <a href="#" className={Styled.headerLink}> سوالات متداول </a> </li>
+                    <li> <a href="#" className={Styled.headerLink}> وبلاگ </a> </li>
+                    <li> <a href="#" className={Styled.headerLink}> آدرس </a> </li>
+                    <li> <a href="#" className={Styled.headerLink}> خدمات در محل </a> </li>
+                    <li> <a href="#" className={Styled.headerLink}> ترمیم شیشه اتوموبیل </a> </li>
+                    <li> <a href="#" className={Styled.headerLink}> تعویض شیشه اتوموبیل </a> </li>
+                </ul>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+
+        </div>
+
+
+    )
+}
+
+export default Header;
